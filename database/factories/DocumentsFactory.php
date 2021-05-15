5debea700789a447e2aad727c015cd469011659b
Tomi <tomi.b.atba@gmail.com>
2021-05-15 23:47:33 +0200

    docker de jajas

--- database/factories/DocumentsFactory.php
+++ database/factories/DocumentsFactory.php
@@ -6,7 +6,7 @@ $factory->define(App\Document::class, function (Faker $faker) {
     return [
         'name' => $faker->sentence(6, true),
         'description' => $faker->paragraph,
-        'url' => 'documents/' . $faker->file('/var/www/academia/.documents', '/var/www/academia/storage/app/public/documents', false),
+        'url' => 'documents/' . $faker->file('/var/www/.documents', '/var/www/storage/app/public/documents', false),
         'subject_id' => $faker->numberBetween(1, 5)
     ];
 });
